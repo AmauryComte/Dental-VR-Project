0ba52e81e40a884a55e99873ebc061fe872f4ab4
hyundokim <hyundkim@users.noreply.github.com>
2018-07-19 17:34:35 +0900

    ClipBoard UI
    open hyundo/opening scene.
    
    if you press button "B" clipboard will be on the left hand.

---
+++ Assets/hyundo/DentalUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DentalUI : MonoBehaviour {
+
+    public Vector3 offset=Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            transform.position = GameObject.FindGameObjectWithTag("lhand").transform.position;
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+        if(OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) < 0.7f && transform.parent != null)
+        {
+            transform.parent = null;
+        }
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "lhand"&&OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger)>0.7f)
+        {
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.7f&&transform.parent!=other.transform)
+            {
+                transform.parent = other.transform;
+            }
+        }
+    }
+}
---
+++ Assets/hyundo/UIHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class UIHandler : MonoBehaviour {
+    public GameObject clipboard;
+
+    private Transform[] texts;
+    private int settingMode=0;
+	// Use this for initialization
+	void Start () {
+        texts = clipboard.GetComponentsInChildren<Transform>() ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (settingMode == 0)
+        {
+            if (other.tag == "TEXT1")
+            {
+                texts[1].GetComponent<TextMeshPro>().text = "ANESTHESIA";
+                texts[2].GetComponent<TextMeshPro>().text = "SCAILING";
+                texts[3].GetComponent<TextMeshPro>().text = "REMOVE TOOTH";
+                settingMode++;
+            }
+        }else if (settingMode == 1)
+        {
+            if (other.tag == "TEXT1")
+            {
+                SceneManager.LoadScene("Dental_Room/scene/dental_room");
+                
+            }
+        }
+    }
+}
